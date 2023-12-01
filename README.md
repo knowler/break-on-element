7ab25e62ee0d5e77629b86c1a6e0ae437f923deb
Nathan Knowler <nathan@knowler.dev>
2023-12-01 15:25:34 -0600

    Add custom element, readme, and demo

---
+++ README.md
@@ -0,0 +1,47 @@
+# Break On Element
+
+A custom element that is a declarative `MutationObserver` for debugging.
+Instead of opening your developer tools to set these breakpoints wrap
+the element in the `<break-on>` custom element.
+
+## Defining
+
+```javascript
+import { BreakOnElement } from "https://esm.sh/gh/knowler/break-on-element.js";
+
+BreakOnElement.define();
+```
+
+## Usage
+
+```html
+<break-on attributes>
+	<p>Will break on all attribute modifications for this element.</p>
+</break-on>
+
+<break-on attributes="class">
+	<p>Will break on class name modifications for this element.</p>
+</break-on>
+
+<break-on attributes="class id">
+	<p>Will break on class name and ID modifications for this element.</p>
+</break-on>
+
+<break-on attributes="class id" subtree>
+	<p>Will break on class name and ID modifications for this element and its subtree.</p>
+</break-on>
+
+<break-on children>
+	<p>Will break on adding and removing direct descendants.</p>
+</break-on>
+
+<break-on children subtree>
+	<p>Will break on adding and removing all descendants.</p>
+</break-on>
+```
+
+## Future
+
+- Options for only additions or deletions of attributes and children.
+- Options for monitoring text changes. I believe `MutationObserver`’s
+	`characterData` option might allow us to do this.
---
+++ break-on-element.js
@@ -0,0 +1,66 @@
+const breakOnElementStyleSheet = new CSSStyleSheet();
+await breakOnElementStyleSheet.replace(`:host { display: contents }`);
+
+export class BreakOnElement extends HTMLElement {
+	#observer;
+
+	get elementToObserve() {
+		return this.querySelector(":scope > :only-child");
+	}
+
+	connectedCallback() {
+		if (!this.shadowRoot) {
+			this.attachShadow({ mode: "open" });
+			this.shadowRoot.adoptedStyleSheets = [breakOnElementStyleSheet];
+			this.shadowRoot.innerHTML = "<slot></slot>";
+		}
+
+		if (this.elementToObserve) {
+			const options = {};
+
+			if (this.hasAttribute("subtree")) {
+				options.subtree = true;
+			}
+
+			if (this.hasAttribute("attributes")) {
+				options.attributes = true;
+				options.attributeOldValue = true;
+				const attributesValue = this.getAttribute("attributes");
+				const isBooleanAttribute = attributesValue === "" || attributesValue === "attributes";
+				if (!isBooleanAttribute) {
+					options.attributeFilter = attributesValue.split(" ");
+				}
+			}
+
+			if (this.hasAttribute("children")) {
+				options.childList = true;
+			}
+
+			// TODO: does characterData allow us to support text changes?
+			//if (this.hasAttribute("text")) {
+			//	options.characterData = true;
+			//	options.characterDataOldValue = true;
+			//}
+
+			if (options.attributes || options.childList || options.characterData) {
+				this.#observer = new MutationObserver(mutationRecords => {
+					debugger;
+				});
+				this.#observer.observe(this.elementToObserve, options);
+			} else {
+				console.warn("Not observing: insufficient options supplied");
+			}
+		}
+	}
+
+	disconnectedCallback() {
+		this.#observer.disconnect();
+	}
+
+	static define(tagName = "break-on") {
+		if (!window.customElements.get(tagName)) {
+			window[this.name] = this;
+			window.customElements.define(tagName, this);
+		}
+	}
+}
